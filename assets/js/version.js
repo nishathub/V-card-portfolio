dccc028a7d72345fa0218863cb7c27130e03f07c
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2023-09-23 16:42:19 +0600

    everything is done: responsiveness, two themes and a switcher. the switcher is needed modify when comes to small device. it will be updated in the next commit

---
+++ assets/js/version.js
@@ -0,0 +1,12 @@
+
+  // Add a click event listener to the version button
+  document.getElementById('version_btn').addEventListener('click', function() {
+    // Get the body element
+    var body = document.body;
+
+    // Toggle the 'theme_2' class on the body element
+    body.classList.toggle('theme_2');
+  });
+
+
+
